a967dccce8c9505dc9736ebed8340b0b1ab2189e
TalhaR <delta.talha@gmail.com>
2019-09-06 20:07:35 -0400

    done

---
+++ Project2/Animal.cpp
@@ -0,0 +1,37 @@
+// Name: Talha Rahman
+// Course: CSCI-235     Professor Ligorio
+// Project 2
+
+// This is the implementation of the Animal class
+#include "Animal.hpp" 
+
+Animal::Animal(){
+    // giving the object default values to make sure no errors 
+    // in case the getters were called
+    name = "";
+    domestic = false;
+    predator = false;
+}
+
+Animal::Animal(std::string name, bool domestic, bool predator)
+: name(name), domestic(domestic), predator(predator) {}
+
+std::string Animal::getName() const {
+    return name;
+}
+bool Animal::isDomestic() const{
+    return domestic;
+}
+bool Animal::isPredator() const{
+    return predator;
+}
+
+void Animal::setName(std::string name){
+    this->name = name;
+}
+void Animal::setDomestic(){ // sets Animal as Domesticated 
+    this->domestic = true;
+}
+void Animal::setPredator(){ // sets Aniaml as a Predator 
+    this->predator = true;
+}
