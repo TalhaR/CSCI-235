8afe8a94731e0fce31bb68696261d7ddd8a5002a
TalhaR <delta.talha@gmail.com>
2019-09-22 10:46:38 -0400

    baseline for project3

---
+++ Project3/Mammal.cpp
@@ -0,0 +1,56 @@
+// Name: Talha Rahman
+// Course: CSCI-235     Professor Ligorio
+// Project 2
+
+// This is the implementation of the Mammal class
+#include "Mammal.hpp"
+
+// calling the superconstructor from Animal because it has the same signature
+Mammal::Mammal(std::string name, bool domestic, bool predator)
+: Animal(name, domestic, predator) {}
+
+// accessor functions
+bool Mammal::hasHair() const {
+    return hair_;
+}
+bool Mammal::isToothed() const {
+    return toothed_;
+}
+bool Mammal::isAirborne() const {
+    return airborne_;
+}
+bool Mammal::isAquatic() const {
+    return aquatic_;
+}
+bool Mammal::hasFins() const {
+    return fins_;
+}
+bool Mammal::hasTail() const {
+    return tail_;
+}
+int Mammal::legs() const {
+    return legs_;
+}
+
+// mutator functions
+void Mammal::setHair(){
+    hair_ = true;
+}
+void Mammal::setToothed(){
+    toothed_ = true;
+}
+void Mammal::setAirborne(){
+    airborne_ = true;
+}
+void Mammal::setAquatic(){
+    aquatic_ = true;
+}
+void Mammal::setFins(){
+    fins_ = true;
+}
+void Mammal::setTail(){
+    tail_ = true;
+}
+void Mammal::setLegs(int legs){
+    legs_ = legs;
+}
---
+++ Project3/main.cpp
@@ -0,0 +1,11 @@
+#include "Animal.hpp"
+#include "Mammal.hpp"
+#include "Bird.hpp"
+#include "Fish.hpp"
+#include <iostream>
+using std::cout;
+
+int main(){
+    
+
+}
