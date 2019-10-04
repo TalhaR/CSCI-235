a75c175be0598038351e0562a9a4aa8049d3eb76
TalhaR <delta.talha@gmail.com>
2019-10-04 11:56:54 -0400

    added Mammal display override

--- Project4/Mammal.cpp
+++ Project4/Mammal.cpp
@@ -4,6 +4,7 @@
 
 // This is the implementation of the Mammal class
 #include "Mammal.hpp"
+#include <iostream>
 
 // calling the superconstructor from Animal because it has the same signature
 Mammal::Mammal(std::string name, bool domestic, bool predator)
@@ -56,5 +57,55 @@ void Mammal::setLegs(int legs){
 }
 
 void Mammal::display() {
-    // TO DO
+    
 }
+
+// void Mammal::display() {
+//     std::cout << this->getName();
+//     if (this->isDomestic()){
+//         std::cout << " is domestic and ";
+//     } else {
+//         std::cout << " is not domestic and ";
+//     }
+//     if (this->isPredator()){
+//         std::cout << "it is ";
+//     } else {
+//         std::cout << "is not ";
+//     }
+//     std::cout << "a predator,\n";
+//     if (this->isAirborne()){
+//         std::cout << "it is airborne ";
+//     } else {
+//         std::cout << "It is not airborne ";
+//     }
+//     if (this->isAquatic()){
+//         std::cout << "it is aquatic,\n";
+//     } else {
+//         std::cout << "It is not aquatic,\n";
+//     }
+//     if (this->hasHair()){
+//         std::cout << "it has hair, ";
+//     } else {
+//         std::cout << "It has no hair, ";
+//     }
+//     if (this->isToothed()){
+//         std::cout << "teeth, ";
+//     } else {
+//         std::cout << "no teeth, ";
+//     }
+//     if (this->hasFins()){
+//         std::cout << "fins, ";
+//     } else {
+//         std::cout << "no fins, ";
+//     }
+//     if (this->hasTail()){
+//         std::cout << "tail, ";
+//     } else {
+//         std::cout << "no tail, ";
+//     }
+//     if (this->legs()){
+//         std::cout << "and " << legs() << " legs.\n\n";
+//     } else {
+//         std::cout << "no legs.\n\n";
+//     }
+// }
