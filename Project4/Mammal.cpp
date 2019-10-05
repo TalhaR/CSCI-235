45135cbcb209eb76257458903d1a566d181f7b24
TalhaR <delta.talha@gmail.com>
2019-10-05 17:32:13 -0400

    finished display method

--- Project4/Mammal.cpp
+++ Project4/Mammal.cpp
@@ -57,55 +57,51 @@ void Mammal::setLegs(int legs){
 }
 
 void Mammal::display() {
-    
+    std::cout << this->getName();
+    if (this->isDomestic()){
+        std::cout << " is domestic and ";
+    } else {
+        std::cout << " is not domestic and ";
+    }
+    if (this->isPredator()){
+        std::cout << "it is ";
+    } else {
+        std::cout << "is not ";
+    }
+    std::cout << "a predator,\n";
+    if (this->isAirborne()){
+        std::cout << "it is airborne ";
+    } else {
+        std::cout << "It is not airborne ";
+    }
+    if (this->isAquatic()){
+        std::cout << "it is aquatic,\n";
+    } else {
+        std::cout << "It is not aquatic,\n";
+    }
+    if (this->hasHair()){
+        std::cout << "it has hair, ";
+    } else {
+        std::cout << "It has no hair, ";
+    }
+    if (this->isToothed()){
+        std::cout << "teeth, ";
+    } else {
+        std::cout << "no teeth, ";
+    }
+    if (this->hasFins()){
+        std::cout << "fins, ";
+    } else {
+        std::cout << "no fins, ";
+    }
+    if (this->hasTail()){
+        std::cout << "tail, ";
+    } else {
+        std::cout << "no tail, ";
+    }
+    if (this->legs()){
+        std::cout << "and " << legs() << " legs.\n\n";
+    } else {
+        std::cout << "no legs.\n\n";
+    }
 }
-
-// void Mammal::display() {
-//     std::cout << this->getName();
-//     if (this->isDomestic()){
-//         std::cout << " is domestic and ";
-//     } else {
-//         std::cout << " is not domestic and ";
-//     }
-//     if (this->isPredator()){
-//         std::cout << "it is ";
-//     } else {
-//         std::cout << "is not ";
-//     }
-//     std::cout << "a predator,\n";
-//     if (this->isAirborne()){
-//         std::cout << "it is airborne ";
-//     } else {
-//         std::cout << "It is not airborne ";
-//     }
-//     if (this->isAquatic()){
-//         std::cout << "it is aquatic,\n";
-//     } else {
-//         std::cout << "It is not aquatic,\n";
-//     }
-//     if (this->hasHair()){
-//         std::cout << "it has hair, ";
-//     } else {
-//         std::cout << "It has no hair, ";
-//     }
-//     if (this->isToothed()){
-//         std::cout << "teeth, ";
-//     } else {
-//         std::cout << "no teeth, ";
-//     }
-//     if (this->hasFins()){
-//         std::cout << "fins, ";
-//     } else {
-//         std::cout << "no fins, ";
-//     }
-//     if (this->hasTail()){
-//         std::cout << "tail, ";
-//     } else {
-//         std::cout << "no tail, ";
-//     }
-//     if (this->legs()){
-//         std::cout << "and " << legs() << " legs.\n\n";
-//     } else {
-//         std::cout << "no legs.\n\n";
-//     }
-// }
